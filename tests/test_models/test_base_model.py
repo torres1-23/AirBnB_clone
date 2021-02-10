7c995b3dc0d7ee10f77143fbfde51a2413c1e6b0
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-02-10 20:21:19 +0000

    Added tests folder with tests for base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module tests base_model.py file.
+
+Usage:
+    To be used with the unittest module, can be use it with
+    "python3 -m unittest discover tests" command or
+    "python3 -m unittest tests/test_models/test_base_model.py"
+"""
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBase01(unittest.TestCase):
+    """Check instantiation of BaseModel instances."""
+    def test_01(self):
+        """Checks right isntantiation of BaseModel"""
+        base1 = BaseModel()
+        self.assertEqual(base1.__class__.__name__, 'BaseModel')
+        self.assertEqual(type(base1), BaseModel)
+        base1.name = "Holberton"
+        base1.number = "89"
+        self.assertEqual(base1.name, "Holberton")
+        self.assertEqual(base1.number, "89")
+
+    def test_02(self):
+        base2 = BaseModel()
