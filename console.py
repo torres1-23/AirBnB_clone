0c24f256c3d37db0c1c3cef9a047a78c1200dbd4
Andy Lopez <profesor.de.ingles@aol.com>
2021-02-12 19:11:05 -0500

    Imported classes

--- console.py
+++ console.py
@@ -9,16 +9,14 @@ import shlex
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
+from models.engine.file_storage import classes
 
-classes = {
-    "BaseModel": BaseModel,
-    "User": User
-    #"State": State,
-    #"City": City,
-    #"Amenity": Amenity,
-    #"Place": Place
-    }
 class_dict = storage.all()
 
 
