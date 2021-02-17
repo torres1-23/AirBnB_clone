dd39a0d44117cb580828a0b12b3ba42712cfa483
Andy Lopez <profesor.de.ingles@aol.com>
2021-02-17 09:31:37 -0500

    Added extra tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -208,7 +208,39 @@ class TestFileStorage03(unittest.TestCase):
             pass
         FileStorage._FileStorage__objects = {}
 
-FileStorage._FileStorage__objects = {}
+
+my_dict = {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
+           'created_at': '2017-09-28T21:03:54.052298',
+           '__class__': 'BaseModel', 'my_number': 89,
+           'updated_at': '2017-09-28T21:03:54.052302',
+           'name': 'Holberton'}
+
+
+class TestFileStorage(unittest.TestCase):
+    """Unittest for file_storage.py"""
+    storage = FileStorage()
+    path = storage._FileStorage__file_path
+    bm_instance = BaseModel(**my_dict)
+    storage.new(bm_instance)
+
+    def test_storage_isinstance(self):
+        """Tests if storage is an instance of FileStorage"""
+        self.assertIsInstance(TestFileStorage.storage, FileStorage)
+
+    def test_file_json(self):
+        """Tests for path existence"""
+        TestFileStorage.storage.save()
+        self.assertTrue(os.path.exists(TestFileStorage.path))
+
+    def test_save_another_instance(self):
+        """Tests for save another instance in path"""
+        bm2_instance = BaseModel()
+        bm2_instance.save()
+        key = type(bm2_instance).__name__ + "." + str(bm2_instance.id)
+        with open(TestFileStorage.path, mode="r", encoding="utf-8") as f:
+            reader = json.load(f)
+        self.assertEqual(
+            reader[key], TestFileStorage.storage.all()[key].to_dict())
 
 if __name__ == "__main__":
     unittest.main()
